6c716c14c2c804c2f90902eaa995e6d938ee9da4
dcdnce <pforesti@student.42nice.fr>
2023-10-18 15:30:58 +0200

    🎨 createCubeMesh() added

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -22,15 +22,12 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
 
-// #include <glm/glm.hpp> // vec2, vec3, mat4, radians
-// #include <glm/gtc/matrix_transform.hpp>
-// #include <glm/gtc/type_ptr.hpp>
-
 #include "pfm/pfm.hpp"
 
 void	processInput(GLFWwindow *w);
 void 	mouseCallback(GLFWwindow* window, double currMouseX, double currMouseY);
 void	loadTexture_jpg(GLuint *texture, const char* filename, GLenum activeTexture);
+Mesh createCubeMesh(void);
 
 pfm::vec3 cubePositions[] = {
     pfm::vec3( 0.0f,  0.0f,  0.0f), 
@@ -63,6 +60,128 @@ int	main(void)
 		return (-1);
 
 	// Create cube mesh
+	Mesh cubeMesh = createCubeMesh();
+
+	// Enable z-buffer
+	glEnable(GL_DEPTH_TEST);
+
+	glUseProgram(gl.program);
+
+	// Load texture
+	loadTexture_jpg(&texture1, "container.jpg", GL_TEXTURE0);
+	glUniform1i(glGetUniformLocation(gl.program, "texture1"), 0);
+
+	// Projection Matrix
+	pfm::mat4	proj = pfm::perspective(pfm::radians(90.f), (float)W_WIDTH/(float)W_HEIGHT, 0.1f, 100.f);
+	glUniformMatrix4fv(glGetUniformLocation(gl.program, "proj"), 1, GL_FALSE, &proj);
+	glUseProgram(0);
+		
+	// Main loop
+	while (!glfwWindowShouldClose(gl.window))
+	{
+		float	currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		processInput(gl.window);
+		glClearColor(0.3f, 0.49f, 0.66f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Bind texture, use program
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glUseProgram(gl.program);
+
+		// Update viewMatrix uniform
+		pfm::mat4	viewMatrix = camera.getViewMatrix();
+		glUniformMatrix4fv(glGetUniformLocation(gl.program, "view"), 1 , GL_FALSE, &viewMatrix);
+
+		// Draw cubes
+		for (size_t i = 0 ; i < 10 ; i++)
+		{
+			pfm::mat4 model = pfm::translate(pfm::mat4(1.f), cubePositions[i]);
+			model = pfm::rotate(model, (float)glfwGetTime() * pfm::radians(20.f * i + 1), pfm::vec3(0.5f, 1.0f, 0.0f));
+			glUniformMatrix4fv(glGetUniformLocation(gl.program, "model"), 1, GL_FALSE, &model);
+			cubeMesh.Draw();
+		}
+
+		// Unbind
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, 0);
+
+        glfwSwapBuffers(gl.window);
+        glfwPollEvents();
+	}
+
+    glDeleteProgram(gl.program);
+	// todo // delete texture
+	return (0);
+}
+
+void	processInput(GLFWwindow *w)
+{
+	if (glfwGetKey(w, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(w, true);
+	if (glfwGetKey(w, GLFW_KEY_W) == GLFW_PRESS)
+		camera.processKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(w, GLFW_KEY_S) == GLFW_PRESS)
+		camera.processKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(w, GLFW_KEY_A) == GLFW_PRESS)
+		camera.processKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(w, GLFW_KEY_D) == GLFW_PRESS)
+		camera.processKeyboard(RIGHT, deltaTime);
+}
+
+void mouseCallback(GLFWwindow* window, double currMouseX, double currMouseY)
+{
+	static bool firstMouse = true;
+	static float lastMouseX;
+	static float lastMouseY;
+    float xpos = static_cast<float>(currMouseX);
+    float ypos = static_cast<float>(currMouseY);
+
+    if (firstMouse)
+    {
+        lastMouseX = xpos;
+        lastMouseY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastMouseX;
+    float yoffset = lastMouseY - ypos; // reversed since y-coordinates go from bottom to top
+    lastMouseX = xpos;
+    lastMouseY = ypos;
+
+	camera.processMouseMovement(xoffset, yoffset);
+}
+
+void	loadTexture_jpg(GLuint *texture, const char* filename, GLenum activeTexture)
+{
+	glGenTextures(1, texture);
+	glActiveTexture(activeTexture);
+	glBindTexture(GL_TEXTURE_2D, *texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// stbi Load
+	unsigned char	*data;
+	int				width, height, nbrChannels;
+	data = stbi_load(filename, &width, &height, &nbrChannels, 0);
+	if (data) {
+		/* Add(!) & Configure texture	*/
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+		std::cout << "main.cpp::stbi_load::couldn't load texture into data\n" << std::endl;
+	stbi_image_free(data);
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+Mesh createCubeMesh(void)
+{
 	std::vector<Vertex> vertices;
 	std::vector<Texture> textures;
 	Vertex	currVertex;
@@ -181,122 +300,5 @@ int	main(void)
 	currVertex.texCoords = pfm::vec2(0.f, 1.f);
 	vertices.push_back(currVertex);
 
-	Mesh cubeMesh(vertices, textures);
-
-	// Enable z-buffer
-	glEnable(GL_DEPTH_TEST);
-
-	glUseProgram(gl.program);
-
-	// Load texture
-	loadTexture_jpg(&texture1, "container.jpg", GL_TEXTURE0);
-	glUniform1i(glGetUniformLocation(gl.program, "texture1"), 0);
-
-	// Projection Matrix
-	pfm::mat4	proj = pfm::perspective(pfm::radians(90.f), (float)W_WIDTH/(float)W_HEIGHT, 0.1f, 100.f);
-	glUniformMatrix4fv(glGetUniformLocation(gl.program, "proj"), 1, GL_FALSE, &proj);
-	glUseProgram(0);
-		
-	// Main loop
-	while (!glfwWindowShouldClose(gl.window))
-	{
-		float	currentFrame = glfwGetTime();
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
-
-		processInput(gl.window);
-		glClearColor(0.3f, 0.49f, 0.66f, 1.f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		// Bind texture, use program
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture1);
-		glUseProgram(gl.program);
-
-		// Update viewMatrix uniform
-		pfm::mat4	viewMatrix = camera.getViewMatrix();
-		glUniformMatrix4fv(glGetUniformLocation(gl.program, "view"), 1 , GL_FALSE, &viewMatrix);
-
-		// Draw cubes
-		for (size_t i = 0 ; i < 10 ; i++)
-		{
-			pfm::mat4 model = pfm::translate(pfm::mat4(1.f), cubePositions[i]);
-			model = pfm::rotate(model, (float)glfwGetTime() * pfm::radians(20.f * i + 1), pfm::vec3(0.5f, 1.0f, 0.0f));
-			glUniformMatrix4fv(glGetUniformLocation(gl.program, "model"), 1, GL_FALSE, &model);
-			cubeMesh.Draw();
-		}
-
-		// Unbind
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, 0);
-
-        glfwSwapBuffers(gl.window);
-        glfwPollEvents();
-	}
-
-    glDeleteProgram(gl.program);
-	// todo // delete texture
-	return (0);
-}
-
-void	processInput(GLFWwindow *w)
-{
-	if (glfwGetKey(w, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(w, true);
-	if (glfwGetKey(w, GLFW_KEY_W) == GLFW_PRESS)
-		camera.processKeyboard(FORWARD, deltaTime);
-	if (glfwGetKey(w, GLFW_KEY_S) == GLFW_PRESS)
-		camera.processKeyboard(BACKWARD, deltaTime);
-	if (glfwGetKey(w, GLFW_KEY_A) == GLFW_PRESS)
-		camera.processKeyboard(LEFT, deltaTime);
-	if (glfwGetKey(w, GLFW_KEY_D) == GLFW_PRESS)
-		camera.processKeyboard(RIGHT, deltaTime);
-}
-
-void mouseCallback(GLFWwindow* window, double currMouseX, double currMouseY)
-{
-	static bool firstMouse = true;
-	static float lastMouseX;
-	static float lastMouseY;
-    float xpos = static_cast<float>(currMouseX);
-    float ypos = static_cast<float>(currMouseY);
-
-    if (firstMouse)
-    {
-        lastMouseX = xpos;
-        lastMouseY = ypos;
-        firstMouse = false;
-    }
-
-    float xoffset = xpos - lastMouseX;
-    float yoffset = lastMouseY - ypos; // reversed since y-coordinates go from bottom to top
-    lastMouseX = xpos;
-    lastMouseY = ypos;
-
-	camera.processMouseMovement(xoffset, yoffset);
-}
-
-void	loadTexture_jpg(GLuint *texture, const char* filename, GLenum activeTexture)
-{
-	glGenTextures(1, texture);
-	glActiveTexture(activeTexture);
-	glBindTexture(GL_TEXTURE_2D, *texture);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	// stbi Load
-	unsigned char	*data;
-	int				width, height, nbrChannels;
-	data = stbi_load(filename, &width, &height, &nbrChannels, 0);
-	if (data) {
-		/* Add(!) & Configure texture	*/
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-		std::cout << "main.cpp::stbi_load::couldn't load texture into data\n" << std::endl;
-	stbi_image_free(data);
-	glBindTexture(GL_TEXTURE_2D, 0);
+	return (Mesh(vertices, textures));
 }
